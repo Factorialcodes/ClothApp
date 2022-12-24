c52b00e0b516359e6fa90cec44660c030853cfc7
factorialcodes <danieloguntolu@gmail.com>
2022-12-24 12:13:33 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+
+import HomePage from './Components/Homepage/homepage.components';
+function App() {
+  return (
+    <div >
+      <HomePage/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Homepage/homepage.components.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='homepage'>
+        <div className='container'>
+
+        </div>
+    </div>
+  )
+}
+
+export default HomePage;
